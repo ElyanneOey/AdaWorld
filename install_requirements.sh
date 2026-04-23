7690b29a9c944fd13e841792a7244211e75aee03
ElyanneOey <99719450+ElyanneOey@users.noreply.github.com>
2026-04-23 11:00:53 +0200

    add install scripts

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 #SBATCH --partition=gpu_mig
-#SBATCH --job-name=install_env
+#SBATCH --job-name=install_req
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=00:30:00
-#SBATCH --output=logs/install_env_%A.out
+#SBATCH --output=logs/install_req_%A.out
 
 module purge
 module load 2025
 module load Anaconda3/2025.06-1
 
-# create env
-conda create -n adaworld_elyanne python=3.10 -y
 
 # activate env
 source activate adaworld_elyanne
 
 # install requirements
-# pip install -r requirements.txt
+pip install -r requirements.txt
