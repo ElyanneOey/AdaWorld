43416cf0574a53a0f8acfb5bbe6f696763581928
ElyanneOey <99719450+ElyanneOey@users.noreply.github.com>
2026-04-23 14:08:39 +0200

    set up tools force reinstall

--- install_requirements.sh
+++ install_requirements.sh
@@ -16,7 +16,7 @@ module load Anaconda3/2025.06-1
 source activate adaworld_elyanne
 
 # setuptools provides pkg_resources, needed by lightning
-pip install setuptools
+pip install --force-reinstall setuptools
 
 # install PyTorch with CUDA 11.8 first (not in requirements.txt)
 # pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
