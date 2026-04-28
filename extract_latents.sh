3c86ddf7c445356e273676ef705ecabd4fe15c58
ElyanneOey <99719450+ElyanneOey@users.noreply.github.com>
2026-04-28 19:52:11 +0200

    extra time

--- extract_latents.sh
+++ extract_latents.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=extract_latents
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=00:50:00
+#SBATCH --time=04:00:00
 #SBATCH --output=logs/extract_latents_%A.out
 
 module purge
