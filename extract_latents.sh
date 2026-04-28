359dea26a17111376974b928c34e34224c8cfddc
ElyanneOey <99719450+ElyanneOey@users.noreply.github.com>
2026-04-28 19:11:53 +0200

    path

--- extract_latents.sh
+++ extract_latents.sh
@@ -13,7 +13,7 @@ module load 2024
 module load Anaconda3/2024.06-1
 source activate adaworld_elyanne
 
-VIDEO_PATH=/home/scur0531/random_actions_data/dataset/retro_act_v0.0.0
+VIDEO_PATH=/home/scur0531/random_actions_data/dataset/retro_act_v0.0.0_random
 CHECKPOINT=${SLURM_SUBMIT_DIR}/checkpoints/lam.ckpt
 
 python New_stuff/extract_latent_actions.py --video $VIDEO_PATH \
