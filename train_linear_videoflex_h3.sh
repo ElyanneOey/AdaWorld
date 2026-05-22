c4a878e35d2114235913d8c225f94e9a9374f655
ElyanneOey <99719450+ElyanneOey@users.noreply.github.com>
2026-05-22 09:58:28 +0200

    3 hidden layers

---
+++ train_linear_videoflex_h3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_h100
+#SBATCH --gpus=1
+#SBATCH --job-name=train_linear_p2p_ada
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=01:00:00
+#SBATCH --output=logs/train_linear_video_h1_%A.out
+
+module purge
+module load 2024
+module load Anaconda3/2024.06-1
+source activate adaworld_elyanne
+
+cd ${SLURM_SUBMIT_DIR}
+
+python New_stuff/train_linear.py \
+    --epochs 100 \
+    --batch_size 256 \
+    --action_hidden_layers 3 \
+    --game_hidden_layers 0 \
+    --dump-dir /gpfs/home3/scur0531/AdaWorld/latent_actions_videoflextok \
+    --no-source \
+    --per_game \
+    --out-csv ./results/videoflextok/per_game_action_accuracy_h3.csv
